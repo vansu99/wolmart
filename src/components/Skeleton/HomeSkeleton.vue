83cf66a84ac6eb0c20a7b5bd744d26954c8a5b36
ntmd-npk <ntmd.npk@gmail.com>
2022-02-14 03:19:28 +0700

    feat(home-sections) using vue-skeleton and change the category images

---
+++ src/components/Skeleton/HomeSkeleton.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="home-skeleton">
+    <div class="home-skeleton__top">
+      <div class="home-skeleton__title">
+        <vue-skeleton-loader width="100%" height="50px" rounded />
+      </div>
+      <div><vue-skeleton-loader width="150px" height="30px" rounded /></div>
+    </div>
+    <div class="home-skeleton__bottom">
+      <div class="home-skeleton__sidebar">
+        <vue-skeleton-loader width="100%" height="100%" rounded />
+      </div>
+      <div class="home-skeleton__content">
+        <skeleton :count="8" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Skeleton from '@/components/Skeleton';
+export default {
+  name: 'CustomSkeleton',
+  props: {},
+  components: { Skeleton },
+};
+</script>
+
+<style lang="scss" scoped>
+.home-skeleton {
+  &__top {
+    margin: 4rem 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 2rem;
+  }
+  &__title {
+    width: 40rem;
+  }
+  &__bottom {
+    display: grid;
+    grid-template-columns: 3fr 9fr;
+    grid-gap: 2rem;
+    @media #{$info-screen-1200} {
+      grid-template-columns: 1fr 3fr;
+    }
+    @media #{$info-screen-992} {
+      grid-template-columns: 1fr 2fr;
+    }
+
+    @media #{$info-screen-575} {
+      grid-template-columns: 1fr;
+    }
+  }
+  &__sidebar {
+    @media #{$info-screen-575} {
+      height: 30rem;
+    }
+  }
+  &__content {
+  }
+}
+</style>
--- src/pages/Home/home.vue
+++ src/pages/Home/home.vue
@@ -1,25 +1,45 @@
 <template>
   <div class="home">
     <Banner />
-    <TopCategories />
-    <Clothing v-for="item in categories" :key="item.id" :category="item" />
+    <template v-if="isShow">
+      <div class="container">
+        <category-skeleton :count="6" :category="true" />
+      </div>
+    </template>
+    <category-list v-else />
+    <template v-if="isShow">
+      <div class="container">
+        <product-skeleton :count="6" :category="true" />
+      </div>
+    </template>
+    <ProductList v-else v-for="item in categories" :key="item.id" :category="item" />
     <Brand />
   </div>
 </template>
 
 <script>
 import { categoryApis } from '@/apis';
-import TopCategories from './components/TopCategories/TopCategories';
+import CategorySkeleton from '@/components/Skeleton';
+import ProductSkeleton from '@/components/Skeleton/HomeSkeleton';
+import CategoryList from './components/TopCategories/TopCategories';
 import Brand from './components/Brand/Brand';
 import Banner from './components/Banner/Banner';
-import Clothing from './components/CategoryProductList/CategoryProductList';
+import ProductList from './components/CategoryProductList/CategoryProductList';
 export default {
   name: 'home',
+  data() {
+    return {
+      isShow: false,
+    };
+  },
+
   components: {
-    TopCategories,
+    CategorySkeleton,
+    ProductSkeleton,
+    CategoryList,
     Brand,
     Banner,
-    Clothing,
+    ProductList,
   },
   computed: {
     categories() {
@@ -33,12 +53,15 @@ export default {
     // call API to get category list
     async getCategories() {
       try {
+        this.isShow = true;
         const categoryData = await categoryApis.getCategoryList();
         if (categoryData.status === 200) {
           await this.$store.dispatch('category/getCategories', categoryData.data);
         }
       } catch {
         console.log('error!!!');
+      } finally {
+        this.isShow = false;
       }
     },
   },
