a0da63b06abbc7d299d0b680b5d881ef4fb89440
su.tran <vansutran99@gmail.com>
2022-02-28 12:38:06 +0700

    fix login testing

--- test/wrapper.js
+++ test/wrapper.js
@@ -1,13 +1,11 @@
 import Vuetify from 'vuetify';
 import Vuex from 'vuex'
-import VueRouter from "vue-router";
 import { createLocalVue } from '@vue/test-utils';
 import { ValidationObserver, ValidationProvider } from 'vee-validate';
 
 const localVue = createLocalVue();
 localVue.use(Vuetify);
 localVue.use(Vuex)
-localVue.use(VueRouter)
 localVue.component('ValidationObserver', ValidationObserver)
 localVue.component('ValidationProvider', ValidationProvider)
 
